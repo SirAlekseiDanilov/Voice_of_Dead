7c93c5ff906871d9ee9b1f06003264cc006c91ac
Danilov Aleksei <iirannok@gmail.com>
2025-08-04 14:22:04 +0300

    Fixet_1

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,28 +4,29 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     public int currentHealth;
+    private int health;
+    //[SerializeField] private int health;
     public float flashSpeed = 5;
-    //public Slider healthSlider;
+    public Slider healthSlider;
     public Color flashColour = new Color(1, 0, 0, 0.1f);
-    private bool isDead;
-    private bool damaged;
 
     private void Awake()
     {
-        currentHealth = 60;
-        damaged = false;
-        isDead = false;
+
+        healthSlider = GameObject.FindGameObjectWithTag("UI_H").GetComponent<Slider>();
+    }
+    private void Start() {
+        health = currentHealth;
     }
 
-    private void Update()
-    {
-        //damaged = false;
-    }
-
+ 
     public void TakeDamage(int amount)
     {
-        damaged = true;
         currentHealth -= amount;
-        //healthSlider.value = currentHealth;
+        float sl = currentHealth / (float)health;
+        print(currentHealth);
+        print(health);
+        print(sl);
+        healthSlider.value = sl;
     }
 }
