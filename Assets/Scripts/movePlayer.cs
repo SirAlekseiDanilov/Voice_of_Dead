58c567beb62cf1e28e9b69e9dfc05066b3731d6a
Danilov Aleksei <iirannok@gmail.com>
2025-08-06 17:32:31 +0300

    poisk

--- Assets/Scripts/movePlayer.cs
+++ Assets/Scripts/movePlayer.cs
@@ -6,7 +6,11 @@ public class movePlayer : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed = 0.5f;
-    private Vector2 moveVector;
+    [SerializeField] private Vector2 moveVector;
+    /// <summary>
+    /// Направление (вращение) игрока.
+    /// </summary>
+    private Vector2 faceRotation;
 
     void Awake()
     {
@@ -15,8 +19,59 @@ public class movePlayer : MonoBehaviour
 
     void Update()
     {
+        UserPress();
+        if (moveVector != Vector2.zero) {
+            Move();
+            Rotator();
+        }
+    }
+    /// <summary>
+    /// Пользовательский ввод.
+    /// </summary>
+    private void UserPress() {
         moveVector.x = Input.GetAxis("Horizontal");
         moveVector.y = Input.GetAxis("Vertical");
+    }
+    /// <summary>
+    /// Передвижение.
+    /// </summary>
+    private void Move() {
         rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+    }
+    /// <summary>
+    /// Вращение.
+    /// </summary>
+    private void Rotator() {
+
+
+
+        /*
+        // GeometryKrash.Vehicle
+        rb.AddTorque(TorqueControl * m_Mobility * Time.fixedDeltaTime, ForceMode2D.Force);
+        rb.AddTorque(-rb.angularVelocity * (m_Mobility / m_MaxAngularVelocity) * Time.fixedDeltaTime, ForceMode2D.Force);
+        */
+
+
+        //transform.right = rb.velocity.normalized;
+        // transform.rotation = Quaternion.LookRotation(moveVector, Vector3.up);
+
+        /*
+        Vector2 correct = Vector2.zero;
+        correct.x = Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad);
+        correct.y = Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad);
+        Debug.DrawRay(transform.position, correct, color: Color.blue, 1);
+        */
+
+        /*
+        Vector3 worldDirectionToPointForward = rb.velocity.normalized;
+        Vector3 localDirectionToPointForward = Vector3.right;
+
+        Vector3 currentWorldForwardDirection = transform.TransformDirection(
+                localDirectionToPointForward);
+        float angleDiff = Vector3.SignedAngle(currentWorldForwardDirection,
+                worldDirectionToPointForward, Vector3.forward);
+
+        transform.Rotate(Vector3.forward, angleDiff, Space.World);
+        */
     }
 }
