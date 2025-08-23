2a2ff6075bd26289c9cc71b86c037535283a4bb5
Danilov Aleksei <iirannok@gmail.com>
2025-08-23 11:57:31 +0300

    t3

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,47 +2,49 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
-{
-    [SerializeField] private int currentHealth;
-    /// <summary>
-    /// Очки жизни.
-    /// </summary>
-    public int HitPoints => currentHealth;
-    /// <summary>
-    /// Максимально возможное кол-во очков жизни (на старте).
-    /// </summary>
-    private int maxHealth;
-    /// <summary>
-    /// Слайдер кол-ва жизней Игрока в UI.
-    /// </summary>
-    public Slider healthSlider;
+namespace VoD {
 
-    /*
-    private void Awake() {
-        healthSlider = GameObject.FindGameObjectWithTag("UI_H").GetComponent<Slider>();
+    public class Health : MonoBehaviour {
+        [SerializeField] private int currentHealth;
+        /// <summary>
+        /// Очки жизни.
+        /// </summary>
+        public int HitPoints => currentHealth;
+        /// <summary>
+        /// Максимально возможное кол-во очков жизни (на старте).
+        /// </summary>
+        private int maxHealth;
+        /// <summary>
+        /// Слайдер кол-ва жизней Игрока в UI.
+        /// </summary>
+        public Slider healthSlider;
+
+        /*
+        private void Awake() {
+            healthSlider = GameObject.FindGameObjectWithTag("UI_H").GetComponent<Slider>();
+        }
+        */
+
+        private void Start() {
+            maxHealth = currentHealth;
+        }
+
+
+        public void TakeDamage(int amount) {
+            currentHealth -= amount;
+            healthSlider.value = currentHealth / (float)maxHealth;
+            if (currentHealth <= 0) {
+                Dead();
+            }
+            ;
+        }
+
+        private void Dead() {
+            Destroy(gameObject);
+            EventDeath?.Invoke();
+        }
+
+        [SerializeField] private UnityEvent EventDeath;
+        public UnityEvent EventOnDeath => EventDeath;
     }
-    */
-
-    private void Start() {
-        maxHealth = currentHealth;
-    }
-
- 
-    public void TakeDamage(int amount)
-    {
-        currentHealth -= amount;
-        healthSlider.value = currentHealth / (float)maxHealth; 
-        if (currentHealth <= 0) {
-            Dead();
-        } ;
-    }
-
-    private void Dead() {
-        Destroy(gameObject);
-        EventDeath?.Invoke();
-    }
-
-    [SerializeField] private UnityEvent EventDeath;
-    public UnityEvent EventOnDeath => EventDeath;
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoD {
+
+    public class Player : MonoBehaviour {
+        [SerializeField] private Health health;
+
+        void Start() {
+
+        }
+
+    }
+}
