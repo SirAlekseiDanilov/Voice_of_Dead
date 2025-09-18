439a43f2cc6b8c0df8b008d280d8441cc066d15f
Danilov Aleksei <iirannok@gmail.com>
2025-09-18 20:41:42 +0300

    dest

--- Assets/Scripts/Weapon_Fist.cs
+++ Assets/Scripts/Weapon_Fist.cs
@@ -14,7 +14,41 @@ namespace VoD {
         [ContextMenu("Attack!")]
         public override void Attack() {
             //Console.WriteLine("Удар кулаком !!!");
-            print("Удар кулаком !!!");
+            if(potential == true) {
+                print("Удар кулаком !!!");
+            }
+
         }
+
+        [SerializeField] private Destructible destruct;
+        [SerializeField] private bool potential = false;
+
+        private void OnTriggerEnter(Collider other) {
+            //destruct = none;
+            destruct = other.gameObject.GetComponent<Destructible>();
+            potential = true;
+        }
+        private void OnTriggerExit(Collider other) {
+            potential = false;
+        }
+
+
+        /*
+         * https://hatchjs.com/how-to-check-if-a-collider-is-colliding-unity/
+        private void OnTriggerStay(Collider other) {
+            if (other.gameObject.tag == "FireZone") {
+                Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+                GetComponent<MeshRenderer>().material.color = color;
+            }
+        }
+
+        // Проверка столкновений между коллайдером и лучом raycast
+        if (collider.Raycast(ray, out hit)) {
+        // Луч raycast попал в коллайдер
+        }
+
+        */
+        //OverlapBox 
+        //if (collider1.IsColliding(collider2)) {
     }
 }
